e7c402c66b131700727d4c49cac60a1e00146c1c
stefanlenz0 <stefle@online.de>
2022-02-27 20:25:55 +0100

    hallo

---
+++ blink/blink.ino
@@ -0,0 +1,19 @@
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+
+  String input;
+  if (Serial.available()) {
+    input = Serial.readStringUntil('\n');
+  }
+  for (int i = 0, x = input.length(); i < x; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay((input[i] - '0') * 1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  } 
+}
