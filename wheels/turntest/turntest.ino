72fd8d5a64940f0219bec210caca76fec68a08f6
stefanlenz0 <stefle@online.de>
2022-03-18 19:53:24 +0100

    bugfixes in turntest :)

--- wheels/turntest/turntest.ino
+++ wheels/turntest/turntest.ino
@@ -1,17 +1,17 @@
 /*
- * A couple simple functions to test using the motor shield with
- * two wheels on seperate motors.
- */
+   A couple simple functions to test using the motor shield with
+   two wheels on seperate motors.
+*/
 
-
+int state = 0;
 // Pins fürs (?) Rad (Channel A)
 int dirA = 12;
-int pwmA = 3;
-int brkeA = 9;
+int pwrA = 3;
+int brkA = 9;
 
 // Pins fürs (?) Rad (Channel)
 int dirB = 13;
-int pwmB = 11;
+int pwrB = 11;
 int brkB = 8;
 
 // define work duties
@@ -22,15 +22,15 @@ int w_high = 100;
 
 
 void setup() {
- 
+
   // --- define pins --- //
 
   pinMode(dirA, OUTPUT);
-  pinMode(pwmA, OUTPUT);
+  pinMode(pwrA, OUTPUT);
   pinMode(brkA, OUTPUT);
 
   pinMode(dirB, OUTPUT);
-  pinMode(pwmB, OUTPUT);
+  pinMode(pwrB, OUTPUT);
   pinMode(brkB, OUTPUT);
 
   // ------------------- //
@@ -39,17 +39,24 @@ void setup() {
 
 
 void loop() {
-
-
-
+  slow();
+  delay(2000);
+  brake();
+  delay(1000);
+  fast();
+  delay(2000);
+  brake();
+  delay(1000);
 }
 
 void brake() {
 
-  digitalWrite(brakePin, HIGH);
-  analogWrite(pwmPin, 0);  
+  digitalWrite(brkA, HIGH);
+  analogWrite(pwrA, 0);
+  digitalWrite(brkB, HIGH);
+  analogWrite(pwrB, 0);
   state = 0;
-  
+
 }
 
 void reverse() {
@@ -57,32 +64,32 @@ void reverse() {
   // don't look this is not good code
 
   // standing
-  if(state == 0)
+  if (state == 0)
     return;
 
   digitalWrite(brkA, LOW);
   digitalWrite(brkB, LOW);
 
   // forward
-  if(state == 1) {
-  
+  if (state == 1) {
+
     digitalWrite(dirA, LOW);
     digitalWrite(dirB, LOW);
     state = 2;
-  
-  } else if(state == 2) {
-    
+
+  } else if (state == 2) {
+
     digitalWrite(dirA, HIGH);
     digitalWrite(dirB, HIGH);
     state = 1;
-    
-  } else if(state == 3)
-      turnB();
 
-    else if(state == 4)
-      turnA();
-    
-  
+  } else if (state == 3)
+    turnB();
+
+  else if (state == 4)
+    turnA();
+
+
 }
 
 void slow() {
@@ -102,7 +109,7 @@ void fast() {
 
   analogWrite(pwrA, w_high);
   analogWrite(pwrB, w_high);
-  
+
 }
 
 void turnA() {
@@ -111,13 +118,13 @@ void turnA() {
   digitalWrite(brkB, LOW);
 
   digitalWrite(dirA, HIGH);
-  analogWrite(pwmPin, 30);
-  
+  analogWrite(pwrA, w_low);
+
   digitalWrite(dirB, LOW);
-  digitalWrite(pwrB, LOW);
+  analogWrite(pwrB, w_low);
 
   state = 3;
-  
+
 }
 
 
@@ -133,5 +140,5 @@ void turnB() {
   digitalWrite(pwrA, LOW);
 
   state = 4;
-  
+
 }
